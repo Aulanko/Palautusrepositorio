b1204220688217cbd7c6be5c845b00819a784992
pronom8 <mangros19@gmail.com>
2025-10-05 18:39:45 +0300

    parannettu koodin selkeyttä sekä tehty 2.17 asti (2.17 tehtynä)

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,45 +2,11 @@ import { useState,useEffect } from 'react'
 import axios from 'axios'
 import palvelin from './services/puhuPalvelinta'
 import './App.css'
+import { Search, Personform, FilteredPersons,Notification } from './components'
 
 
 
-const Search = ({search,handleSearch}) =>{
-  return(
-    <h4>Search bar: <input value={search} onChange={handleSearch}></input></h4>
-  )
-}
 
-const Personform = ({handleSubmit, newName, handleNameChange, newNumber, handleNumberChange}) =>{
-  return(
-    <form onSubmit={handleSubmit}>
-
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          Number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-  )
-}
-
-const FilteredPersons = ({filteredPersons, handleRemove}) =>{
-  return(
-    <>
-    {filteredPersons.map((person,index)=>(
-      <div key={index}>
-        <h3 key={index}>{person.name} {person.number}<button onClick={()=>handleRemove(person)}>delete</button></h3>
-        
-      </div>
-      ))}
-
-    </>
-  )
-}
 
 function App() {
   const [persons, setPersons] = useState([
@@ -50,12 +16,17 @@ function App() {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+  const [message, setMessage] = useState('')
+  const [messageType, setMessageType] = useState('')
 
- useEffect(()=>{
-  palvelin.getAll().then(response=>{
-      setPersons(response.data)
-    })
- },[])
+ useEffect(() =>{
+  palvelin.getAll().then(response =>{
+    setPersons(response.data)
+  }).catch(error =>{
+    console.log('Jokin meni vikaan, kun yritettiin päivittää sovellusta. Kenties samanaikaista muutosta', error)
+  })
+}, [])
+ 
   
   const handleNameChange = (e) =>{
     setNewName(e.target.value)
@@ -76,25 +47,39 @@ function App() {
     e.preventDefault()
     const existingPerson = persons.find(person => person.name==newName)
     if(existingPerson){
-      palvelin.update({...existingPerson, number:newNumber}).then(response=>{
-      setPersons(persons.map(p=>
-        p.id==existingPerson.id?response.data:p
-      ))
-
-    })
-
-      alert(`${newName}'s number updated`)
+      palvelin.update({...existingPerson, number:newNumber}).then(response => {
+        setPersons(persons.map(p => 
+          p.id == existingPerson.id ? response.data : p
+        ))
+      }).catch(error => {
+        setMessage(`Virhe: ${newName} was aldready removed from the server`)
+        setMessageType('error')
+        console.log('Päivitys ei onnistunut, jokin meni vikaan. Sovellus ei tue kunnolla saman aikaisia muutoksia', error)
+      })
+      setMessage(`${newName}'s number updated`)
+      setMessageType('updated')
+      setTimeout(() => {
+        setMessage(null)
+      }, 2000)
       return
     }
-
-
 
     palvelin.create({name: newName, number:newNumber}).then(response=>{
       setPersons([...persons,response.data])
 
-    })
+    }).catch(error => {
+    setMessage(`Error: Could not add ${newName}`)
+    setMessageType('error')
+    console.log('Yhtäaikaista muutosta sovelluksessa tai jokin meni vikaan', error)
+  })
+    setMessage(
+          `${newName}'s number added`
+        )
+    setMessageType('success')
+        setTimeout(() => {
+          setMessage(null)
+        }, 2000)
 
-    //setPersons([...persons, {name:newName, number:newNumber}])
    
   }
 
@@ -102,16 +87,31 @@ function App() {
     if(window.confirm(`Delete ${person.name}`)){
        palvelin.remove(person).then(()=>{
         setPersons(persons.filter(p=>p.name!==person.name))
-       })
+        setMessage(
+          `${newName}'s number removed`
+        )
+        setMessageType('success')
+              setTimeout(() => {
+                setMessage(null)
+              }, 2000)
+
+       }
+      
+      ).catch(error=>{
+        setMessage(`Error: ${person.name} was already deleted from server`)
+        setMessageType('error')
+        console.log("jotain meni vikaan poistaessa", error)
+      })
     }
-   
-   
+
+    
 
   }
 
   return (
     <>
       <h1>Phonebook</h1>
+      <Notification message={message} messageType={messageType}/>
       <Search search={search} handleSearch={handleSearch}/>
       
 
