ad021382808b024c1699dea0e8b33dfbb33233f6
pronom8 <mangros19@gmail.com>
2025-10-06 16:34:56 +0300

    2.18 nimien näyttämis toiminnallisuus lisätty

--- osa2/maidenTiedot/src/App.jsx
+++ osa2/maidenTiedot/src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
 import './App.css'
 
+const Maantiedot = ({country}) =>{
+
+  return(
+    <>
+      <h2>{country.name.common}</h2>
+     
+
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+  const [inputa, setInputa] = useState('')
+  const [maat, setMaat] = useState([])
+
+  useEffect(()=>{
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(response=>{
+      setMaat(response.data)})
+  }, [])
+
+  const handleInput = (e) =>{
+    e.preventDefault()
+    setInputa(e.target.value)
+  }
+
+  const filteredMaat = maat.filter(country=>
+    country.name.common.toLowerCase().includes(inputa.toLowerCase())
+  )
+
+
 
   return (
     <>
       <div>
-        <h3>Find countries <input></input></h3>
+        <h3>Find countries <input onChange={handleInput}></input></h3>
       </div>
+      
+      {filteredMaat.length>10? <p>too many matches, spesify more</p>:
+      filteredMaat.map(country =>(
+        <Maantiedot key={maat.cca3} country={country} />
+      ))}
       
     </>
   )
