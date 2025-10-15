5a0d0c9718aec140e97bf31955d303d7c99aec49
pronom8 <mangros19@gmail.com>
2025-10-15 15:39:11 +0300

    renamed the puhelinluettelo folder to dist folder

---
+++ osa3/FrontendPuhelinluettelo/dist/src/services/puhuPalvelinta.js
@@ -0,0 +1,34 @@
+import axios from "axios"
+
+
+const baseUrl = import.meta.env.VITE_NODE_ENV === 'development' 
+  ? 'http://localhost:3001/api/persons'
+  : 'https://backi-puhelin-applikaatio-leo.onrender.com/api/persons'
+  
+
+const getAll= ()=>{
+    return axios
+    .get(baseUrl)
+    
+}
+
+const create = (person)=>{
+    return axios
+    .post(baseUrl, person)
+}
+
+const update = (person) =>{
+    return axios.put(`${baseUrl}/${person.id}`,person)
+}
+
+const remove = (person) =>{
+    return axios.delete(`${baseUrl}/${person.id}`)
+}
+
+export default{
+    getAll:getAll,
+    create: create,
+    remove: remove,
+    update:update
+
+}
