49bc3039d791f3e418226e5e5af9cc40adb6f919
pronom8 <mangros19@gmail.com>
2025-10-25 13:26:29 +0300

    4.8 tehty

--- osa4/blogilista/utils/BlogiListanTestit.test.js
+++ osa4/blogilista/utils/BlogiListanTestit.test.js
@@ -1,11 +1,38 @@
 
-const {test, describe, after} = require('node:test')
+const {test, describe, after, beforeEach} = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const { application } = require('express')
 const app = require('../index')
+const Blog = require('../models/blogs')
+const  assert  = require('node:assert')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    "title": "Hepan juoksu",
+    "author": "Heppa",
+    "url": "http://koira.fi",
+    "likes": 42,
+    "id": "68f0e71dd4dbd29615557037"
+  },
+  {
+    "title": "Kissan seisonta",
+    "author": "Kissa",
+    "url": "http://kissa.fi",
+    "likes": 12,
+    "id": "68f0e71dd4dbd29615557099"
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 
 describe('tietokannasta hakemistestejä', ()=>{
@@ -17,6 +44,13 @@ describe('tietokannasta hakemistestejä', ()=>{
 
         
     })
+
+    test('All blogs returned', async() =>{
+        const res = await api.get('/api/blogs')
+    
+        assert.strictEqual(initialBlogs.length, res.body.length)
+    })
+
     after(async () =>{
         await mongoose.connection.close()
     })
