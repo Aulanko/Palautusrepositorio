0e0b787b470259e2e171b19a9522973052129f61
pronom8 <mangros19@gmail.com>
2026-06-14 20:04:19 +0300

    4.16 actually done, added validation to controller

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -10,7 +10,10 @@ usersRouter.post('/', async (request, response) => {
   
   const { username, name, password } = request.body
 
-  
+ 
+  if(username.length<3||password.length<3){
+    return response.status(400).send({error:"username or password too short"})
+  }
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
